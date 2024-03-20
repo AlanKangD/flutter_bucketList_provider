7449424c05f15f2ecbe25fb4a184a7dc731aab7d
alan <kno12350@gmail.com>
2024-03-20 14:14:25 +0900

    provider 패키지를 사용하여 버킷리스트 update 로직 구현

--- lib/bucket_service.dart
+++ lib/bucket_service.dart
@@ -7,9 +7,15 @@ class BucketService extends ChangeNotifier {
   List<Bucket> bucketList = [
     Bucket('잠자기', false), // 더미(dummy) 데이터
   ];
-
+// Bucket 추가
   void createBucket(String job) {
     bucketList.add(Bucket(job, false));
     notifyListeners(); // 모든 Consumer<BucketService> 의  builder 함수 호출 하는 메소드
+  }
+
+// Bucket 수정
+  void updateBucket(Bucket bucket, int index) {
+    bucketList[index] = bucket;
+    notifyListeners();
   }
 }
